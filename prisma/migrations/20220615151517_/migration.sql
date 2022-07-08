d91d465a6ff9da011b09ffe7d725ac93c95c7c03
Rasla <programmer.rasla@gmail.com>
2022-07-08 11:04:40 +0530

    new chat ui

--- prisma/migrations/20220615151517_/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Room" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "membersCount" INTEGER NOT NULL DEFAULT 0,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
-);
--- utils/helpers.js
+++
@@ -1,3 +0,0 @@
-export const checkLength = (arr) => {
-  return arr.length > 0;
-};
